27ae894870b7dc311cea49feb5c5fc99b57ab235
3ds129alejandroCampos <20253ds129@utez.edu.mx>
2026-06-04 12:41:00 -0600

    Creacion de modelo de usuariosDao ->clase para registrar usuario

---
+++ src/java/main/controller/RegistroUsuarioServlet.java
@@ -0,0 +1,10 @@
+package main.controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/registro")
+public class RegistroUsuarioServlet extends HttpServlet {
+
+
+}
