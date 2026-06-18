696518ea6190ae13b16e33fe9cb9391e369bc483
Tu Nombre Apellido <tu-correo@ejemplo.com>
2026-06-18 10:22:01 -0600

    [NEW] publicar servicio

---
+++ src/main/webapp/assets/js/serviciosExtra.js
@@ -0,0 +1,33 @@
+const uploadBox = document.getElementById("uploadBox");
+const fileInput = document.getElementById("fileInput");
+const preview = document.getElementById("preview");
+
+uploadBox.addEventListener("click", () => {
+    fileInput.click();
+});
+
+fileInput.addEventListener("change", mostrarImagenes);
+
+function mostrarImagenes() {
+
+    preview.innerHTML = "";
+
+    Array.from(fileInput.files).forEach(file => {
+
+        const reader = new FileReader();
+
+        reader.onload = function(e){
+
+            const img = document.createElement("img");
+
+            img.src = e.target.result;
+
+            preview.appendChild(img);
+
+        }
+
+        reader.readAsDataURL(file);
+
+    });
+
+}
