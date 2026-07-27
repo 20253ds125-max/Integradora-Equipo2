4b614e373b7f7ce8315876ac89c8f7d0e242458f
Emiliano_2E <20253ds125@utez.edu.mx>
2026-07-27 00:38:31 -0600

    Apartado de mesas: Acomo de codigo a model

---
+++ src/main/java/com/eventonline/model/Mesas.java
@@ -0,0 +1,69 @@
+package com.eventonline.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Mesas {
+
+    private int idMesa;
+    private String nombre;
+    private int capacidad;
+    private int idUsuario;
+    private List<Invitado> invitados = new ArrayList<>();
+
+    public Mesas(){
+
+    }
+    public Mesas(String nombre, int capacidad, int idUsuario){
+        setNombre(nombre);
+        setCapacidad(capacidad);
+        this.idUsuario = idUsuario;
+    }
+
+    public int getIdMesa(){
+        return  idMesa;
+    }
+
+    public void setIdMesa(int idMesa){
+        this.idMesa =idMesa;
+    }
+
+    public String getNombre() {
+        return  nombre;
+    }
+
+    public void setNombre(String nombre){
+        if (nombre == null || nombre.trim().isEmpty()){
+            throw new IllegalArgumentException("El nombre de la mesa es obligatorio");
+        }
+        if (nombre.length()>100){
+            throw new IllegalArgumentException("El nombre de la mesa es demasiado largo");
+
+        }
+        this.nombre = nombre.trim();
+    }
+
+    public int getCapacidad(){
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad){
+        if(capacidad == 10){
+            throw  new IllegalArgumentException("Capacidad maxima de 10 personas");
+        }
+        this.capacidad= capacidad;
+    }
+
+    public int getIdUsuario(){
+        return  idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario){
+        this.idUsuario = idUsuario;
+    }
+
+    public  void setInvitados(List<Invitados> invitados){
+        this.invitados = invitados != null ? invitados : new ArrayList<>();
+    }
+
+}
