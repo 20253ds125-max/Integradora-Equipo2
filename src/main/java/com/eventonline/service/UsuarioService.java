7f231959c31647a953f4d1cae99bde23da80ae0e
3ds129alejandroCampos <20253ds129@utez.edu.mx>
2026-07-27 15:01:27 -0600

    [new]agregacion de servlet

--- src/main/java/com/eventonline/service/UsuarioService.java
+++ src/main/java/com/eventonline/service/UsuarioService.java
@@ -2,6 +2,8 @@ package com.eventonline.service;
 
 import com.eventonline.dao.UsuariosDao;
 import com.eventonline.model.Usuario;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 import java.sql.SQLException;
 
@@ -17,4 +19,33 @@ public class UsuarioService {
 
         usuariosDao.guardarUsuario(usuario);
     }
+
+    public void iniciarSesion(String email, String pass, HttpServletRequest request)throws SQLException,IllegalArgumentException{
+        Usuario encontrado= usuariosDao.buscarUsuarioPorCorreo(email);
+        if(encontrado!=null){
+            if(usuariosDao.estadoBloqueado(email)){
+             throw new IllegalArgumentException("Tu cuenta tiene un bloqueo activo intenta mas tarde");
+            }
+            if(usuariosDao.verificarUsuario(email,pass)!=null){
+                usuariosDao.resetearIntentos(email);
+                HttpSession session = request.getSession();
+                session.setAttribute("UsuarioLog",encontrado);
+
+            }
+            else {
+                int intentos = usuariosDao.registrarIntento(email);
+                if(intentos>=3){
+                    usuariosDao.bloquearCuenta(email);
+                    throw new IllegalArgumentException("Tu cuenta tiene un bloqueo activo, intentalo mas tarde");
+                }else {
+                    int intentosRestantes=3-intentos;
+                    throw new IllegalArgumentException("Te restan "+intentosRestantes+" intento(s)");
+                }
+            }
+        }else{
+            throw new IllegalArgumentException("El correo o la contraseña es incorrecta");
+        }
+    }
+
+    // accesibilidad -> public,private,protected static
 }
