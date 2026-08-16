451f70f5136b762063f2fc1fefbd03e1f6a12fe7
3ds129alejandroCampos <20253ds129@utez.edu.mx>
2026-08-16 12:16:46 -0600

    [Modulo de gestion de usuarios y su logica]

---
+++ src/main/java/com/eventonline/controller/admin/administracionUsuarios.java
@@ -0,0 +1,51 @@
+package com.eventonline.controller.admin;
+
+import com.eventonline.model.Usuario;
+import com.eventonline.service.AdminService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet("/adminUsuarios")
+public class administracionUsuarios extends HttpServlet {
+    public final AdminService adminService= new AdminService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession sesion = req.getSession(false);
+
+        if (sesion == null || sesion.getAttribute("UsuarioLog") == null) {
+            req.setAttribute("error", "Por favor inicia sesión para continuar.");
+            req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req, resp);
+            return;
+        }
+
+        Usuario usuarioLogueado = (Usuario) sesion.getAttribute("UsuarioLog");
+
+        if (!"ADMIN".equalsIgnoreCase(usuarioLogueado.getRol())) {
+            req.setAttribute("error", "Acceso denegado. Se requiere cuenta de administrador.");
+            req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req, resp);
+            return;
+        }
+        try{
+            List<Usuario> listaDeUsuarios= adminService.listaUsuarios();
+            req.setAttribute("listaDeUsuarios",listaDeUsuarios );
+            req.getRequestDispatcher("/WEB-INF/usuarios.jsp").forward(req,resp);
+    }catch (SQLException e){
+            req.setAttribute("error","A ocurrido un error al cargar los recintos intenta mas tarde: "+e.getMessage() );
+            req.getRequestDispatcher("/admin-servicios");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req,resp);
+    }
+}
